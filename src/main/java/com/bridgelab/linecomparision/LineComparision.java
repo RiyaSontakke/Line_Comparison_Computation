3280245b2eaaee4227087de89135e5b835018478
RiyaSontakke <riyaharhare04@gmail.comgit config --global user.email riyaharhare04@gmail.com>
2023-01-26 12:44:39 +0530

    Added:Comapre Two Lines

--- src/main/java/com/bridgelab/linecomparision/LineComparision.java
+++ src/main/java/com/bridgelab/linecomparision/LineComparision.java
@@ -1,7 +1,42 @@
 package com.bridgelab.linecomparision;
 
+import java.util.Scanner;
+
 public class LineComparision {
 	public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Program");   
-	}
+        System.out.println("Welcome to Line Comparison Program"); 
+        Line line1 = getLine();
+        double lengthofLine = getLength(line1);
+        System.out.println("length of line 1 is: " + lengthofLine);
+        System.out.println("Enter the value for second line");
+        Line line2 = getLine();
+        double lengthofLine2 = getLength(line2);
+        System.out.println("length of line 1 is " +lengthofLine2);
+        System.out.println(line1);
+        System.out.println(line2);
+        if(lengthofLine == lengthofLine2)
+        	System.out.println("both lines are equal");
+        else if (lengthofLine > lengthofLine2)
+        	System.out.println("Line1 is greater than Line2");
+        else
+            System.out.println("Line2 is greater than Line1");
+	} 
+     public static Line getLine() {
+    	 double x1,y1,x2,y2;
+         Scanner scan = new Scanner(System.in);
+         System.out.println("Enter value for x1");
+         x1 = scan.nextDouble();
+         System.out.println("Enter value for y1");
+         y1 = scan.nextDouble();
+         System.out.println("Enter value for x2");
+         x2 = scan.nextDouble();
+         System.out.println("Enter value for y2");
+         y2 = scan.nextDouble();
+         Line line = new Line(x1,y1,x2,y2);
+         return line;
+     }
+        public static  double getLength(Line line) { 
+        	double lengthofLine = Math.sqrt(Math.pow(line.x2 - line.x1, 2) + Math.pow(line.y2 -line.y1, 2));
+         return lengthofLine;
+ 	}
 }
